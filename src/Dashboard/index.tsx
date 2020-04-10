98a53362feb1456eb2f61be361bc9795c14b243d
Timo Frank <timo.frank@student.uni-luebeck.de>
2020-04-10 13:34:59 +0300

    cleaned up & added csv reading of dataset

--- src/Dashboard/index.tsx
+++ src/Dashboard/index.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import { LineChart, XAxis, CartesianGrid, Line, Tooltip } from 'recharts'
-import { Dropdown, Icon, Menu, Segment } from 'semantic-ui-react'
+import { Dropdown, Menu, Loader } from 'semantic-ui-react'
 import Papa from 'papaparse'
-import axios from 'axios'
 
-const {StringStream} = require("scramjet");
-const request = require("request");
+type dataRow = {
+    year: string,
+    category: string,
+    winner: string,
+    name: string,
+    work: string,
+    'p:gender': string,
+    'p:birthday': string,
+    'p:birthplace': string,
+    'p:deathday': string,
+    'p:popularity': string,
+    'p:biography': string,
+    'p:knownForDepartment': string,
+    'p:profilePath': string,
+    'w:posterPath': string,
+    'w:genreIds': string,
+    'w:popularity': string,
+    'w:overview': string
+}
 
 const data = [
     { name: 'Page A', uv: 1000, pv: 2400, amt: 2400, uvError: [75, 20] },
@@ -20,10 +36,11 @@ const data = [
     { name: 'Page J', uv: 189, pv: 4800, amt: 2400, uvError: [15, 60] },
   ];
 
-const readDataDSV = async (): Promise<any> => {
+const readCSVData = async (filename: string): Promise<dataRow[]> => {
     return new Promise((resolve, reject) => {
         try {
-            Papa.parse('https://github.com/Seemeile/uni-helsinki-prj_movieawards/raw/master/src/data/the_oscar_award.csv', {
+            const githubBase = 'https://raw.githubusercontent.com/Seemeile/uni-helsinki-prj_awards/master/src/data/'
+            Papa.parse(githubBase + filename, {
                 header: true,
                 download: true,
                 skipEmptyLines: true,
@@ -31,7 +48,7 @@ const readDataDSV = async (): Promise<any> => {
                     reject(error)
                 },
                 complete(results) {
-                    resolve(results)
+                    resolve(results.data)
                 },
             })
         } catch (error) {
@@ -40,44 +57,31 @@ const readDataDSV = async (): Promise<any> => {
     })
 }
 
-const readCSV = async (): Promise<any> => {
-    return axios.get("https://dog.ceo/api/breeds/list/all")
-}
-
 export default function Dashboard() {
-    
-    //readDataDSV().then(results => console.log(results))
+    const [pending, setPending] = useState(false)
+    const [dataState, setDataState] = useState([{}])
 
-    readCSV().then(res => console.log(res))
-
-    //console.log(request.get("https://srv.example.com/main.csv"))
-        //.pipe(new StringStream())                       // pass to stream
-        //.CSVParse()                                   // parse into objects
-        //.then(() => console.log("all done"))
+    const handleDatasetOscarAwardClick = useCallback(async () => {
+        setPending(true)
+        const results: dataRow[] = await readCSVData('the_oscar_award.csv')
+        const year1928: dataRow[] = results.filter(row => row.year === '1928') // TEST
+        setDataState(year1928)
+        setPending(false)
+    }, [setDataState])
 
     return (
         <>
             <Menu attached='top'>
-                <Dropdown item icon='wrench' simple>
+                <Dropdown item icon='wrench' simple text='datasets'>
                     <Dropdown.Menu>
-                    <Dropdown.Item>
-                        <Icon name='dropdown' />
-                        <span className='text'>New</span>
-
-                        <Dropdown.Menu>
-                        <Dropdown.Item>Document</Dropdown.Item>
-                        <Dropdown.Item>Image</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown.Item>
-                    <Dropdown.Item>Open</Dropdown.Item>
-                    <Dropdown.Item>Save...</Dropdown.Item>
-                    <Dropdown.Item>Edit Permissions</Dropdown.Item>
-                    <Dropdown.Divider />
-                    <Dropdown.Header>Export</Dropdown.Header>
-                    <Dropdown.Item>Share</Dropdown.Item>
+                        <Dropdown.Item onClick={handleDatasetOscarAwardClick}>Oscar Awards 1928 - 2020</Dropdown.Item>
                     </Dropdown.Menu>
                 </Dropdown>
-
+                {pending ? (
+                    <div style={{marginTop: '8px', marginLeft: '8px'}}>
+                        <Loader active inline size='small' />
+                    </div>)
+                : ''}
                 <Menu.Menu position='right'>
                     <div className='ui right aligned category search item'>
                     <div className='ui transparent icon input'>
@@ -95,14 +99,13 @@ export default function Dashboard() {
             <LineChart
                 width={1200}
                 height={400}
-                data={data}
+                data={dataState}
                 margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
             >
                 <XAxis dataKey="name" />
                 <Tooltip />
                 <CartesianGrid stroke="#f5f5f5" />
-                <Line type="monotone" dataKey="uv" stroke="#ff7300" yAxisId={0} />
-                <Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1} />
+                <Line type="monotone" dataKey="p:birthday" stroke="#ff7300" yAxisId={0} />
             </LineChart>
         </>
     )
