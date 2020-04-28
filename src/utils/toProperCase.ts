030c95e5f5cf4b026bcd1b706e75ca30f947cc60
Timo Frank <timo.frank@student.uni-luebeck.de>
2020-04-28 23:17:23 +0300

    fixed bug with trailing spaces

--- src/utils/toProperCase.ts
+++ src/utils/toProperCase.ts
@@ -1,5 +1,5 @@
 export const toProperCase = (string: string) => {
-    return string.split(' ').map(w => {
+    return string.trim().split(' ').map(w => {
         if (w[0] === '(') {
             return '(' + w[1].toUpperCase() + w.substr(2).toLowerCase()
         }
